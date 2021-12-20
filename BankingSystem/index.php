995bcbf68484711fc92c86f459ea25043dcc2e2d
Sakshi Sunil Gade <87658700+sakkkshiie@users.noreply.github.com>
2021-12-21 00:31:34 +0530

    Add files via upload

--- BankingSystem/index.php
+++ BankingSystem/index.php
@@ -1,1 +1,100 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Basic Bank System</title>
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link rel="stylesheet"type="text/css"href-"style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" >
+<link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@700&display=swap" rel="stylesheet">
+<link rel-"stylesheet"type="text/css"href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/fontawesome.min.css">
+    
+  </head>
+  <body>
+    
+<header id="Home">
+ <nav class="navbar navbar-expand-lg navbar-light bg-white px-5 border-bottom fixed-top">
+  <div class="container-fluid">
+    <a class="navbar-brand fs-2" href="#">SPARK-<span class="text-primary">Bank System</span></a><br>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0 fs-4 ">
+        <li class="nav-item">
+          <a class="nav-link active text-info" aria-current="page" href="index.php">Home</a>
+        </li>
+        
+          <a class="nav-link" href="About Us.php">About Us</a>
+        </li><li class="nav-item">
+          <a class="nav-link" href="Contact Us.php">Contact Us</a></li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="Services.php" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Services
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="user.php">User Accounts</a></li>
+            <li><a class="dropdown-item" href="transfer.php">Transaction</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="history.php">Transaction History</a></li>
+            
+        </li>
+       </ul>
+      </div>
+  </div>
+</nav>
+<br><br>
+<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images/b1.jfif" class="d-block w-100" alt="...">
+      <div class="carousel-caption  d-md-block ">
+        <h5>Person-To-Person</h5>
+        <p>Small step to a better future...!</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="images/b2.jfif" class="d-block w-100" alt="...">
+      <div class="carousel-caption  d-md-block ">
+        <h5>Bank-To-Bank</h5>
+        <p>A fresh approach to banking...!</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="images/b3.jfif" class="d-block w-100" alt="...">
+      <div class="carousel-caption  d-md-block">
+        <h5>Up-To-Date</h5>
+        <p>You are in good hands...!</p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+</header>
 
+>
+
+<br>
+
+ <div class="container-fluid bg-primary text-white p-3 text-center fs-4">Developed by-© 2021.Sakshi Gade<br> For the Project of The Sparks Foundation</div>
+<script 
+ src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script> <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+<script>
+  AOS.init();
+</script>
+</body>
+  </html>
