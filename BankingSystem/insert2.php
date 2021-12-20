547107ffbcd2d64668325d3a54ff44b4e19e0475
Sakshi Sunil Gade <87658700+sakkkshiie@users.noreply.github.com>
2021-12-21 00:33:58 +0530

    Add files via upload

--- BankingSystem/insert2.php
+++ BankingSystem/insert2.php
@@ -1,1 +1,23 @@
+<?php
+$Email=$_POST['Email'];
+$Mobile No=$_POST['Mobile No'];
+$Address=$_POST['Address'];
+$City=$_POST['City'];
+$Zip=$_POST['Zip'];
+$conn=mysqli_connect("localhost","root","","bankingsystem");
+if($conn-> connect_error){
+    die("connection failed:".$conn->connect_error);
+}
+else{
+    $INSERT="INSERT Into contact(Email,Mobile No,Address,City,Zip) values(?,?,?,?,?,?)";
+    $stmt=$conn->prepare($INSERT);
+    $stmt->bind_param("vivvi",$Email,$Mobile No,$Address,$City,$Zip);
+    $stmt->execute();
+    
+}
 
+
+    $stmt->close();
+    $conn->close();
+    
+?>
